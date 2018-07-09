43321a1b1d023e1afb91e0e16bf835a65d45cf9e
justinzyw <justin.zy.wang@gmail.com>
2018-07-09 12:01:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 mkdir /tmp/consul
 
-exec sh -c ./bin/consul agent -data-dir=/tmp/consul -join=devops-prometheus-consul
+nohup /bin/consul agent -data-dir=/tmp/consul \
+-retry-join=devops-prometheus-consul \
+-bind=$(ip -o -4 addr list eth0 | head -n1 | awk '{print $4}' | cut -d/ -f1) \
+-client=$(ip -o -4 addr list eth0 | head -n1 | awk '{print $4}' | cut -d/ -f1) &
 
 exec sh -c ./bin/node_exporter
